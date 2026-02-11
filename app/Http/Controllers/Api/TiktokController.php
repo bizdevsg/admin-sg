f4d4ff10482959ba48e76d3bb33918a78f136daa
Kresno Nugroho <kresnonugroho46@gmail.com>
2026-02-11 10:17:20 +0700

    update

--- app/Http/Controllers/Api/TiktokController.php
+++ app/Http/Controllers/Api/TiktokController.php
@@ -12,7 +12,26 @@ class TiktokController extends Controller
      */
     public function index()
     {
-        $tiktoks = TiktokLink::all();
+        $tiktoks = TiktokLink::all()->map(function ($item) {
+            $embed = (string) ($item->embed_code ?? '');
+            $url = null;
+
+            if ($embed !== '') {
+                if (preg_match('/cite="([^"]+)"/i', $embed, $m)) {
+                    $url = $m[1];
+                } elseif (preg_match('#https?://www\.tiktok\.com/[^\\s"<>]+#i', $embed, $m)) {
+                    $url = $m[0];
+                }
+            }
+
+            return [
+                'id' => $item->id,
+                'title' => $item->title,
+                'url' => $url,
+                'created_at' => $item->created_at,
+                'updated_at' => $item->updated_at,
+            ];
+        });
 
         return response()->json([
             'status' => 200,
